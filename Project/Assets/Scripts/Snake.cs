cae428c6695c5de4e5858bc6cc6253d7d5016fbe
Val Nuñez <val@friendlyhello.com>
2021-06-24 21:34:08 -0700

    Snake moves!
    Added WASD input controls

---
+++ Project/Assets/Scripts/Snake.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    // Vector 2 variable for movement directions
+    private Vector2 _direction;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    
+    private void FixedUpdate()
+    {
+            transform.position = new Vector3(
+            Mathf.Round(transform.position.x) + _direction.x,
+            Mathf.Round(transform.position.y) + _direction.y,
+            0.0f
+            );
+    }
+}
