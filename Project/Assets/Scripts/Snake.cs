4f66989e26ece5456ec663d79e439b19b60b3f0c
Val Nuñez <val@friendlyhello.com>
2021-06-28 19:41:42 -0700

    updating scripts with some comments

--- Project/Assets/Scripts/Snake.cs
+++ Project/Assets/Scripts/Snake.cs
@@ -46,7 +46,8 @@ public class Snake : MonoBehaviour
     
     private void FixedUpdate()  
     {
-        // Loop through the segments in reverse
+        // Loop through the segments in backwards order
+        // It makes sure that each segment added is following the one in front of it
         for (int i = _segment.Count - 1; i > 0; i--)
         {
             _segment[i].position = _segment[i - 1].position;
@@ -63,7 +64,11 @@ public class Snake : MonoBehaviour
 
     private void Grow()
     {
+        // Reference to new segment
         Transform segment = Instantiate(_segmentPrefab);
+
+        // set position of segment at the end of the snake (always add to tail)
+        //  _segment[_segment.Count - 1] will get the last segment prefab
         segment.position = _segment[_segment.Count - 1].position;
 
         _segment.Add(segment);
@@ -71,6 +76,11 @@ public class Snake : MonoBehaviour
 
     private void ResetState()
     {
+        // Clear out list of segments
+
+        // index 1 in int i = 1is the second segment prefab, 0 is the head
+        // and is not to be destroyed! Because the head at index 0 is also
+        // the asset this script is attached to!
         for (int i = 1; i < _segment.Count; i++)
         {
             Destroy(_segment[i].gameObject);
@@ -79,6 +89,7 @@ public class Snake : MonoBehaviour
         _segment.Clear();
         _segment.Add(transform);
 
+        // Set initialize size of snake with a for loop
         for (int i = 1; i < initialSize; i++)
         {
             _segment.Add(Instantiate(_segmentPrefab));
